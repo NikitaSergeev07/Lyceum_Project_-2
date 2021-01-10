ec711d8029b1b694b9743c66aa33a2a69eb16733
NikitaSergeev07 <59967001+NikitaSergeev07@users.noreply.github.com>
2021-01-10 22:02:26 +0300

    Update Ghost.py

--- Ghost.py
+++ Ghost.py
@@ -1,9 +1,3 @@
-import pygame
-import math
-from random import randrange
-import random
-from SETTINGS import screen, square, gameBoard, ElementPath, spriteRatio, spriteOffset, game, ghostGate
-
 # Делал Саша
 class Ghost:
     pass
